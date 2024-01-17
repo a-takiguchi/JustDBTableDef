1d3157b5d0ef6e6eed27b481756b1e472308193f
takiguchi <atsuhiro.takiguchi@beex-inc.com>
2024-01-17 17:23:35 +0900

    feat: add panel and filter info

--- physi_logi_def/tables.go
+++ physi_logi_def/tables.go
@@ -1,9 +1,25 @@
 package physi_logi_def
 
-var tables = map[string]string{
-    "domain-ssl":  "table_1704780398",
+type AccessInfo struct {
+    tableID string
+    filterID string
+    panelID string
 }
 
-func GetLogiName(alias string) string {
-    return tables[alias]
+var tables = map[string]AccessInfo{
+    "domain-ssl": { 
+        tableID: "table_1704780398",
+        filterID: "filter_1704786328",
+        panelID:  "panel_1704780386",
+    },
+}
+
+func GetTableID(alias string) string {
+    return tables[alias].tableID
+}
+func GetPanelID(alias string) string {
+    return tables[alias].panelID
+}
+func GetFilterID(alias string) string {
+    return tables[alias].filterID
 }
