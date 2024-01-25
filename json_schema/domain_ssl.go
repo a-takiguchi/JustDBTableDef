c98915927369e6829fd5de5db99a765d44aec326
takiguchi <atsuhiro.takiguchi@beex-inc.com>
2024-01-25 14:52:50 +0900

    feat: change to done method

--- json_schema/domain_ssl.go
+++ json_schema/domain_ssl.go
@@ -36,6 +36,31 @@
 */
 package json_schema
 
+import "encoding/json"
+
+type DomainSSL struct {
+    TableName string `json:"tableName"`
+    RecordId json.Number `json:"recordId"`
+    Reactions []string `json:"reactions"`
+    Comments []string `json:"comments"`
+    CreatedBy []string `json:"createdBy"`
+    CreatedAt string `json:"createdAt"`
+    UpdatedBy []string `json:"updatedBy"`
+    UpdatedAt string `json:"updatedAt"`
+    WorkflowName string `json:"workflowName"`
+    InsertUser []string `json:"insertUser"`
+    SubstituteUser []string `json:"substituteUser"`
+    NextApprovalUsers []string `json:"nextApprovalUsers"`
+    SubmittedAt string `json:"submittedAt"`
+    LastApprovalUser []string `json:"lastApprovalUser"`
+    ApproveAt []string `json:"approveAt"`
+    ApprovalUser []string `json:"approvalUser"`
+    ApprovalStatus string `json:"approvalStatus"`
+    RecordLink string `json:"recordLink"`
+    // Record json.RawMessage `json:"record"`
+    Record DomainSSLRecord `json:"record"`
+}
+
 type DomainSSLRecord struct {
     Project string `json:"field_1684834053_K"`
     Customer string `json:"field_1670909964_K22"`
